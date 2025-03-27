d6f7f0ecbe10ddafbaba3a9ae61c0e3506316755
junahrpark <qkrwnsdk625@gmail.com>
2025-03-27 11:27:30 +0900

    연습파일

---
+++ Assets/Practice.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //구현 연습하는거임
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
